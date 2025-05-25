9fe11c5aaa51d68316518e256c5b9352dab8020c
IIITGYANIIIT <chandragyan2003@gmail.com>
2025-05-25 13:25:59 +0530

    Navbar added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,90 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {assets} from "../assets/assets";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
-const Navbar = () => {
+const App = () => {
   const [open, setOpen] = React.useState(false);
+  const { user, setUser, setShowUserLogin, navigate } = useAppContext();
+
+  const logout = () => {
+    setUser(null);
+    navigate("/");
+  };
+
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-green-100 relative transition-all">
       <NavLink to="/">
-        <img
-          className="h-9"
-          src="{assets.logo}"
-          alt="logo"
-        />
+        <img className="h-9" src={assets.logo} alt="logo" />
       </NavLink>
+
       {/* Desktop Menu */}
       <div className="hidden sm:flex items-center gap-8">
-        <NavLink
-          to="/"
-          className="font-medium text-green-700 hover:text-green-900"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          className="font-medium text-green-700 hover:text-green-900"
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className="font-medium text-green-700 hover:text-green-900"
-        >
-          Contact
-        </NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">All Products</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
           <input
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
           />
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.836 10.615 15 14.695"
-              stroke="#22c55e"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              clipRule="evenodd"
-              d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783"
-              stroke="#22c55e"
-              strokeWidth="1.2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <img src={assets.search_icon} alt="search" className="w-4 h-4" />
         </div>
         <div className="relative cursor-pointer">
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0"
-              stroke="#22c55e"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <img
+            src={assets.nav_cart_icon}
+            alt="cart"
+            className="w-6 opacity-80"
+          />
           <button className="absolute -top-2 -right-3 text-xs text-white bg-green-600 w-[18px] h-[18px] rounded-full">
             3
           </button>
-        </div>
-        <button className="cursor-pointer px-8 py-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full">
-          Login
-        </button>
+        </div>{" "}
+        {!user ? (
+          <button
+            onClick={() => setShowUserLogin(true)}
+            className="cursor-pointer px-8 py-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full"
+          >
+            Login
+          </button>
+        ) : (
+          <button
+            onClick={logout}
+            className="cursor-pointer px-8 py-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full"
+          >
+            Logout
+          </button>
+        )}
       </div>
       <button
         onClick={() => setOpen(!open)}
@@ -92,52 +63,50 @@ const Navbar = () => {
         className="sm:hidden"
       >
         {/* Menu Icon SVG */}
-        <svg
-          width="21"
-          height="15"
-          viewBox="0 0 21 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect width="21" height="1.5" rx=".75" fill="#22c55e" />
-          <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#22c55e" />
-          <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#22c55e" />
-        </svg>
+        <img src={assets.menu_icon} alt="menu" />
       </button>
-      {/* Mobile Menu */}
-      <div
-        className={`${
-          open ? "flex" : "hidden"
-        } absolute top-[60px] left-0 w-full bg-green-100 shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}
-      >
-        <NavLink
-          to="/"
-          className="block font-medium text-green-700 hover:text-green-900"
-          onClick={() => setOpen(false)}
+      {open && (
+        <div
+          className={`${
+            open ? "flex" : "hidden"
+          } absolute top-[60px] left-0 w-full bg-green-100 shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}
         >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          className="block font-medium text-green-700 hover:text-green-900"
-          onClick={() => setOpen(false)}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className="block font-medium text-green-700 hover:text-green-900"
-          onClick={() => setOpen(false)}
-        >
-          Contact
-        </NavLink>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full text-sm">
-          Login
-        </button>
-      </div>
+          <NavLink to="/" onClick={() => setOpen(false)}>
+            Home{" "}
+          </NavLink>
+          <NavLink to="/products" onClick={() => setOpen(false)}>
+            All Products{" "}
+          </NavLink>
+          {user && (
+            <NavLink to="/my-products" onClick={() => setOpen(false)}>
+              My Orders{" "}
+            </NavLink>
+          )}
+          <NavLink to="/contact" onClick={() => setOpen(false)}>
+            Contact{" "}
+          </NavLink>
+          {!user ? (
+            <button
+              onClick={() => {
+                setOpen(false);
+                setShowUserLogin(true);
+              }}
+              className="cursor-pointer px-6 py-2 mt-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full text-sm"
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              onClick={logout}
+              className="cursor-pointer px-6 py-2 mt-2 bg-green-600 hover:bg-green-700 transition text-white rounded-full text-sm"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
-
+export default App;
